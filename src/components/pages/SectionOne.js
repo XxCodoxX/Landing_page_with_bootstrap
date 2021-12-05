256dd090d32528c06a2c2ed429c3ab55f3ae4c14
Mithun <mithun@xdoto.io>
2021-12-05 17:45:17 +0530

    finel edit

--- src/components/pages/SectionOne.js
+++ src/components/pages/SectionOne.js
@@ -1,29 +1,37 @@
 import React from "react";
-import SectionOneImg from '../../img/sectionOne.png'
+import SectionOneImgOne from "../../img/sectionOneImgOne.png";
+import SectionOneImgSecond from "../../img/SectionOneImgSecond.png";
 import ButtonTypeOne from "./pagesComponents/ButtonTypeOne";
 
 const SectionOne = () => {
   return (
     <>
-      <section class="section-one-box">
-        <div class="section-one-layer">
-          <img src={SectionOneImg} alt="" srcset="" />
+      <section class="header">
+        <div class="gradient"></div>
+
+        <div class="image-layer-1">
+          <img src={SectionOneImgOne} alt="" srcset="" />
         </div>
 
-        <div class="section-one-text">
-            <p class="section-one-heading">Explore your</p>
-            <p class="section-one-secondHeading">Creativity</p>
-            <p class="section-one-para">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua.
+        <div class="corner">
+          <img src={SectionOneImgSecond} alt="" />
+        </div>
+
+        <div class="explore-box">
+          <div class="cent-text">
+            <span class="explore-heading">Explore your</span>
+            <span class="creative-text">Creativity</span>
+
+            <p class="explore-text">
+              Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut.
             </p>
 
             <ButtonTypeOne
-                name = "View All Course"
-                bgColor = "#fecb00"
-                btnCount = "one"
-            />
-
+          name = "View All Course"
+          bgColor = "#fecb00"
+          btnCount = "one"
+          />
+          </div>
         </div>
       </section>
     </>
